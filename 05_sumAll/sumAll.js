dca2f234486299b4c9b2363d31f43f1b85061ab3
MarshallScottInnoPro22 <marshallscottinnopro22@gmail.com>
2024-01-07 16:10:45 -0500

    Created functions to pass tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+const error = "ERROR";
+let sum = 0;
 
+let low = num1 > num2 ? num2 : num1;
+let high = num2 > num1 ? num2 : num1;
+
+if (num1 < 0 || num2 < 0 || typeof num1 != 'number' || typeof num2 != 'number') {
+    return error;
+}
+
+for (i = low; i <= high; i++) {
+   sum = sum + i;
+}
+    return sum;
 };
 
 // Do not edit below this line
